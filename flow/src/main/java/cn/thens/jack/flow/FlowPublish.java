7ff2e0ad2e9a51234a478e42d959ce229a142eb6
7hens <thens@txzing.com>
2020-09-10 17:08:57 +0800

    refactor(flow): FlowPublish

--- flow/src/main/java/cn/thens/jack/flow/FlowPublish.java
+++ flow/src/main/java/cn/thens/jack/flow/FlowPublish.java
@@ -13,26 +13,30 @@ class FlowPublish<T> extends Flow<T> {
     private final Flow<T> upFlow;
     private final Func0<? extends FlowEmitter<T>> emitterFactory;
     private final AtomicBoolean isStarted = new AtomicBoolean(false);
-    private final AtomicReference<FlowEmitter<T>> emitterRef;
+    private final AtomicReference<FlowEmitter<T>> flowEmitterRef;
 
     FlowPublish(Flow<T> upFlow, Func0<? extends FlowEmitter<T>> emitterFactory) {
         this.upFlow = upFlow;
         this.emitterFactory = emitterFactory;
-        this.emitterRef = new AtomicReference<>(Funcs.call(emitterFactory));
+        this.flowEmitterRef = new AtomicReference<>(Funcs.call(emitterFactory));
     }
 
     @Override
     protected void onStartCollect(Emitter<? super T> emitter) throws Throwable {
-        final FlowEmitter<T> flowEmitter = emitterRef.get();
+        final FlowEmitter<T> flowEmitter = flowEmitterRef.get();
         if (isStarted.compareAndSet(false, true)) {
-            upFlow.onStartCollect(flowEmitter);
-            flowEmitter.addCancellable(() -> {
-                FlowEmitter<T> newEmitter = Funcs.call(emitterFactory);
-                if (newEmitter != flowEmitter) {
-                    isStarted.set(false);
-                    emitterRef.set(newEmitter);
+            Emitter<T> newEmitter = createEmitter(emitter, reply -> {
+                flowEmitter.post(reply);
+                if (reply.isCancel() || reply.isError()) {
+                    FlowEmitter<T> newFlowEmitter = Funcs.call(emitterFactory);
+                    if (newFlowEmitter != flowEmitter) {
+                        isStarted.set(false);
+                        flowEmitterRef.set(newFlowEmitter);
+                    }
                 }
             });
+            upFlow.onStartCollect(newEmitter);
+            flowEmitter.addCancellable(newEmitter);
         }
         flowEmitter.asFlow().onStartCollect(emitter);
     }
